13d851bbc04a285361deff2cf02efa5e1a6e5430
Xulav <sulavthapachhetri@gmail.com>
2022-10-14 22:35:55 +0545

    added gitignore

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="sentenceselector",
-    version="1.0.1",
+    version="1.0.2",
     description="This is a sentence selector by words package.",
     long_description="This package invokes all the sentences that contains the word you want. There can be a millions sentences and if a word is used in thousand sentences, all the thousand sentences are displayed by our sentenceselector package.",
     author="Sulav",
